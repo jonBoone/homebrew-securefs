0a150c4bc7552844f0bff8978db52f42c41a7e75
Jon Boone <ipmonger@delamancha.org>
2021-05-10 15:28:05 -0400

    adds securefs.rb formula with a Requirement for osxfuse to be installed on MacOs

---
+++ Formula/securefs.rb
@@ -0,0 +1,55 @@
+class OsxfuseRequirement < Requirement
+  fatal true
+
+  staisfy(build_env: false) { self.class.binary_osxfuse_installed? }
+
+  def self.binary_osxfuse_installed?
+    File.exist?("/usr/local/include/fuse.h") &&
+      !File.symlink?("/usr/local/include/fuse")
+  end
+
+  env do
+    ENV.append_path "PKG_CONFIG_PATH",
+                    "/usr/local/lib/pkgconfig:#{HOMEBREW_PREFIX}/lib/pkgconfig:"\
+                    "#{HOMEBREW_PREFIX}/opt/openssl@1.1/lib/pkgconfig"
+    ENV.append_path "BORG_OPENSSL_PREFIX", "#{HOMEBREW_PREFIX}/opt/openssl@1.1/"
+
+    unless HOMEBREW_PREFIX.to_s == "/usr/local"
+      ENV.append_path "HOMEBREW_LIBRARY_PATHS", "/usr/local/lib"
+      ENV.append_path "HOMEBREW_INCLUDE_PATHS", "/usr/local/include/fuse"
+    end
+  end
+
+   def message
+    "macFUSE is required to build borgbackup-fuse. Please run `brew install --cask macfuse` first."
+  end
+end
+
+class Securefs < Formula
+  desc "Filesystem with transparent authenticated encryption"
+  homepage "https://github.com/netheril96/securefs"
+  url "https://github.com/netheril96/securefs.git",
+      tag:      "0.11.1",
+      revision: "dfeebf8406871d020848edde668234715356158c"
+  license "MIT"
+  head "https://github.com/netheril96/securefs.git"
+
+  depends_on "cmake" => :build
+
+  on_macos do
+    depends_on OsxfuseRequirement => :build
+  end
+
+  on_linux do
+    depends_on "libfuse"
+  end
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/securefs", "version" # The sandbox prevents a more thorough test
+  end
+end

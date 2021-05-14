f23ce0efb0e67784f19991319d997e66b523f1b5
Jon Boone <ipmonger@delamancha.org>
2021-05-14 12:43:42 -0400

    attempts to install from current $HEAD

--- Formula/securefs.rb
+++ Formula/securefs.rb
@@ -28,8 +28,8 @@ class Securefs < Formula
   desc "Filesystem with transparent authenticated encryption"
   homepage "https://github.com/netheril96/securefs"
   url "https://github.com/netheril96/securefs.git",
-      tag:      "0.11.1",
-      revision: "dfeebf8406871d020848edde668234715356158c"
+      tag:      "HEAD",
+      revision: "fac00a59042c33ed20a048e906476246d5377423"
   license "MIT"
   head "https://github.com/netheril96/securefs.git"
 
